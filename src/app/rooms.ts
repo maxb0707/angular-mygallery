087184cd395a019782c6510303e2646f308a5867
maxb0707 <benz.maximilian@gmail.com>
2019-11-18 20:38:54 +0100

    1 file modified

--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -1,6 +1,6 @@
 export const rooms = [
   {
     name: "Upload your first room",
-    photoSrc: "https://www.w3schools.com/images/w3schools_green.jpg"
+    photoSrc: "https://github.com/maxb0707/angular-mygallery/blob/master/src/assets/living-room.JPG?raw=true"
   }
 ];
